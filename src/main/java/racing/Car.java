c550dfb707aa371d15ef0d2156346e92be8b6710
TJOEUNIS <HwaEVE@gmail.com>
2024-12-12 13:52:53 +0900

    car 클레스 완성

--- src/main/java/racing/Car.java
+++ src/main/java/racing/Car.java
@@ -4,29 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
-class Car {
-    public static List<String> car () {
+public class Car {
+    public static List<String> getCarNames( Scanner scanner){
 
-        Scanner scanner = new Scanner(System.in);
-
-        // 자동차 이름 입력 받기
-        System.out.print("자동차 이름을 입력하세요 (쉼표로 구분): ");
         String carNamesInput = scanner.nextLine();
-
-        // 잘못된 입력 검증 및 자동차 이름 리스트 생성
         List<String> carNames = new ArrayList<>();
-        while (true) {
-            if (carNamesInput.isEmpty()) {
-                break;
+
+        for (String name : carNamesInput.split(",")) {
+
+            if (name.length() > 10) {
+                System.out.println("[ERROR] 자동차 이름은 10자 이내여야 합니다.");
+                return null;
             }
-            for (String name : carNamesInput.split(",")) {
-                // 자동차 이름 길이 검사
-                if (name.length() > 10) {
-                    System.out.println("[ERROR] 자동차 이름은 10자 이내여야 합니다.");
-                    continue; // 다음 이름으로 넘어감
-                }
-                carNames.add(name);
-            }
+            carNames.add(name);
         }
         return carNames;
     }
